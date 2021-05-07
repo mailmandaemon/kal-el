c45c7dac13c9769d74187bc5d08178ac35ee9f1a
Liam McCallum <mailman.industries@gmail.com>
2021-05-07 19:52:06 +1000

    cleaned up code

--- commands/command-base.js
+++ commands/command-base.js
@@ -1,4 +1,4 @@
-const { prefix } = require('../config.json')
+const { prefix } = require('../config.json');
 
 const validatePermissions = (permissions) => {
   const validPermissions = [
@@ -33,121 +33,102 @@ const validatePermissions = (permissions) => {
     'MANAGE_ROLES',
     'MANAGE_WEBHOOKS',
     'MANAGE_EMOJIS',
-  ]
+  ];
 
   for (const permission of permissions) {
     if (!validPermissions.includes(permission)) {
-      throw new Error(`Unknown permission node "${permission}"`)
+      throw new Error(`Unknown permission node "${permission}"`);
     }
   }
-}
+};
 
 module.exports = (commandOptions) => {
-  let {
-    commands,
-    permissions = [],
-  } = commandOptions
+  let { commands, permissions = [] } = commandOptions;
 
   // Ensure the command and aliases are in an array
   if (typeof commands === 'string') {
-    commands = [commands]
+    commands = [commands];
   }
 
-  console.log(`Registering command "${commands[0]}"`)
+  console.log(`Registering command "${commands[0]}"`);
 
   // Ensure the permissions are in an array and are all valid
   if (permissions.length) {
     if (typeof permissions === 'string') {
-      permissions = [permissions]
+      permissions = [permissions];
     }
 
-    validatePermissions(permissions)
+    validatePermissions(permissions);
   }
 
   for (const command of commands) {
-      allCommands[command] = {
-          ...commandOptions,
-          commands,
-          permissions,
-      }
+    allCommands[command] = {
+      ...commandOptions,
+      commands,
+      permissions,
+    };
   }
-}
+};
 
-const allCommands = {}
+const allCommands = {};
 
 module.exports.listen = (client) => {
-// Listen for messages
-    client.on('message', (message) => {
-        const { member, content, guild } = message;
-            
-        // Split on any number of spaces
-        const arguments = content.split(/[ ]+/);
+  // Listen for messages
+  client.on('message', (message) => {
+    const { member, content, guild } = message;
 
-        // Remove the command which is the first index
-        const name = arguments.shift().toLowerCase();
+    // Split on any number of spaces
+    const arguments = content.split(/[ ]+/);
 
-        if (name.startsWith(prefix)) {
-            const command = allCommands[name.replace(prefix,'')]
-            return
-        }
+    // Remove the command which is the first index
+    const name = arguments.shift().toLowerCase();
 
-        const { 
-            permissions, 
-            permissionError = "",
-            requiredRoles = [],
-            minArgs = 0,
-            maxArgs = null,
-            expectedArgs,
-            callback,
-        } = command;
-        // A command has been ran
+    if (name.startsWith(prefix)) {
+      const command = allCommands[name.replace(prefix, '')];
+      return;
+    }
 
-            // Ensure the user has the required permissions
-            for (const permission of permissions) {
-                if (!member.hasPermission(permission)) {
-                message.reply(permissionError)
-                return
-                }
-            }
-    
-            // Ensure the user has the required roles
-            for (const requiredRole of requiredRoles) {
-                const role = guild.roles.cache.find(
-                (role) => role.name === requiredRole
-                )
-    
-                if (!role || !member.roles.cache.has(role.id)) {
-                message.reply(
-                    `You must have the "${requiredRole}" role to use this command.`
-                )
-                return
-                }
-            }
-    
-            // Ensure we have the correct number of arguments
-            if (
-                arguments.length < minArgs ||
-                (maxArgs !== null && arguments.length > maxArgs)
-            ) {
-                message.reply(
-                `Incorrect syntax! Use ${name} ${expectedArgs}`
-                )
-                return
-            }
-    
-            // Handle the custom command code
-            callback(message, arguments, arguments.join(' '), client)
-    })
-}
+    const {
+      permissions,
+      permissionError = '',
+      requiredRoles = [],
+      minArgs = 0,
+      maxArgs = null,
+      expectedArgs,
+      callback,
+    } = command;
+    // A command has been ran
 
+    // Ensure the user has the required permissions
+    for (const permission of permissions) {
+      if (!member.hasPermission(permission)) {
+        message.reply(permissionError);
+        return;
+      }
+    }
 
-// for (const alias of commands) {
-//     const command = `${prefix}${alias.toLowerCase()}`
-// 
-//     if (
-//       content.toLowerCase().startsWith(`${command} `) ||
-//       content.toLowerCase() === command
-//     ) {
-//       
-//     }
-//   }
+    // Ensure the user has the required roles
+    for (const requiredRole of requiredRoles) {
+      const role = guild.roles.cache.find((role) => role.name === requiredRole);
+
+      if (!role || !member.roles.cache.has(role.id)) {
+        message.reply(
+          `You must have the "${requiredRole}" role to use this command.`
+        );
+        return;
+      }
+    }
+
+    // Ensure we have the correct number of arguments
+    if (
+      arguments.length < minArgs ||
+      (maxArgs !== null && arguments.length > maxArgs)
+    ) {
+      message.reply(`Incorrect syntax! Use ${name} ${expectedArgs}`);
+      return;
+    }
+
+    // Handle the custom command code
+    callback(message, arguments, arguments.join(' '), client);
+  });
+};
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const path = require('path')
-const fs = require('fs')
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const path = require('path');
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 const config = require('./config.json');
 
 client.on('ready', async () => {
-    console.log('the client is ready');
+  console.log('the client is ready');
 
-    const baseFile = 'command-base.js';
-    const commandBase = require(`./commands/${baseFile}`);
+  const baseFile = 'command-base.js';
+  const commandBase = require(`./commands/${baseFile}`);
 
-    const readCommands = dir => {
-        const files = fs.readdirSync(path.join(__dirname, dir));
-        for (const file of files) {
-            const stat = fs.lstatSync(path.join(__dirname, dir, file));
-            if (stat.isDirectory()) {
-                readCommands(path.join(dir, file))
-            } else if(file !== baseFile) {
-                const option = require(path.join(__dirname, dir, file));
-                commandBase(option)
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir));
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file));
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file));
+      } else if (file !== baseFile) {
+        const option = require(path.join(__dirname, dir, file));
+        commandBase(option);
+      }
+    }
+  };
 
-            }
-        }
-    };
+  readCommands('commands');
+  commandBase.listen(client);
+});
 
-    readCommands('commands');
-    commandBase.listen(client);
-})
-
-client.login(config.token)
+client.login(config.token);
