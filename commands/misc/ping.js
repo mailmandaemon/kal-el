60ae8fa64fdcb59f531f73d1fadaa9e2fecafd8d
Exxon <65795854+Exxonnnnnn@users.noreply.github.com>
2021-05-07 11:01:35 +0100

    There u go

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     commands: 'ping',
     minArgs: 0,
     macArgs: 0,
-    callback: (message, arguments, text) => {
-        message.reply('Pong!')
+    callback: (message, arguments, text, client) => {
+        message.reply(`My ping is ${Math.round(client.ws.ping)}`);
     }
 }
